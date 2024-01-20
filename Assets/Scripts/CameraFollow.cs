117cd26a5951fb4736a887949c7326c91a417987
blessedbygodmonti <150172779+blessedbygodmonti@users.noreply.github.com>
2024-01-20 08:45:17 +0530

    by me

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+    // The Target
+    public Transform target;
+    
+    void LateUpdate () {
+        if (transform == null) return;
+        transform.position = new Vector3(target.position.x,
+                                         transform.position.y,
+                                         transform.position.z);
+    }
+}
